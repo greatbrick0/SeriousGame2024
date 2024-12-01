676932fe26a86e911758597e92f3f433bfe9d6a7
greatbrick <speharley@gmail.com>
2024-12-01 00:50:15 -0500

    Added Hallway

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -1,13 +1,15 @@
 extends Node3D
 
 @export var npcName: String = "Prof. Doctor"
-@export_multiline var npcInfo: String = "Vaping is bad"
+@export_multiline var npcMonologue: String = "Vaping is bad"
+@export var heightMult: float = 1.0
 
 func _ready():
+	$"Standing Idle".scale *= heightMult
 	$InteractArea.hoverText = npcName
 
 func _process(delta):
 	$"Standing Idle/AnimationPlayer".play("mixamo_com")
 
 func _on_interact_area_activated(ref):
-	ref.get_node("Label").text = npcInfo
+	ref.get_node("Label").text = npcMonologue
